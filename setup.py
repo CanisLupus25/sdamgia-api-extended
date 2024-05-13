e55bc7c33a1f0c4c29c7f5bb61c78ce1f88b800f
Yandex Lyceum <iabakulin000@mail.ru>
2024-05-13 16:37:16 +0300

    fixes2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Python модуль для взаимодействия с образовательным порталом СДАМ ГИА',
     long_description=open('README.md', encoding="utf8").read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/anijackich/sdamgia-api',
+    url='https://github.com/CanisLupus25/sdamgia-api-extended',
     license='MIT',
     install_requires=['requests', 'beautifulsoup4', 'pyppeteer', 'grabzit', 'html2image', 'aiohttp'],
     packages = ['sdamgia-extended-extended'],
